6305cf8efbfea9fb2abaf5c15c05e4e2d0b70806
SayoTeKey <sayo.briefs@gmail.com>
2025-06-16 16:34:50 +0200

    Abhängigkeiten in package.json und package-lock.json aktualisiert; JSX-Dateien importiert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-// import { useState } from "react";
-
 import "./App.css";
-import HeaderComponent from "./components/HeaderComponent";
-import MainComponent from "./components/MainComponent";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import MainComponent from "./components/MainComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <HeaderComponent />
