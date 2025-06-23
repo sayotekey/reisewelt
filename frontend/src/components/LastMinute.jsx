34f9384ce1f01a8f993f06adf26b3dc7d54fb3c9
SayoTeKey <sayo.briefs@gmail.com>
2025-06-23 15:54:33 +0200

    LastMinute component updated: Layout and navigation added; new images integrated. LastMinutesDeals page created

--- frontend/src/components/LastMinute.jsx
+++ frontend/src/components/LastMinute.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import beachFivehundredDeals from "../images/beach-fivehundred-deals.jpg";
+import LuggageLastMinute from "../images/luggage-last-minute.jpg";
 
 const LastMinute = () => {
-  return <div>LastMinute</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2 className=" px-7 text-left text-2xl mt-10">
+        Jetzt buchen und bis zu 50% sparen!
+      </h2>
+      <div className="flex">
+        <div className="py-10">
+          <div className="relative">
+            <img
+              src={LuggageLastMinute}
+              alt="Reisekoffer mit Aufklebern"
+              className="w-auto px-7"
+            />
+            <h3 className="absolute left-16 top-10 text-black text-2xl">
+              Super Last Minute
+            </h3>
+            <button
+              className="absolute right-16 bottom-5"
+              onClick={() => navigate("/lastMinuteDeals")}
+            >
+              Jetzt entdecken
+            </button>
+          </div>
+        </div>
+        <div className="py-10">
+          <div className="relative">
+            <img
+              src={beachFivehundredDeals}
+              alt="Strand und Meer mit Badeschuhen und Hut"
+              className="w-auto px-7"
+            />
+            <h3 className="absolute left-16 top-10 text-black text-2xl">
+              Urlaub unter 500€
+            </h3>
+            <button
+              className="absolute right-16 bottom-5"
+              onClick={() => navigate("/fivehundredEuroDeals")}
+            >
+              Jetzt entdecken
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LastMinute;
---
+++ frontend/src/pages/LastMinutesDeals.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LastMinutesDeals = () => {
+  return <div>Liste toller Last Minute Deals</div>;
+};
+
+export default LastMinutesDeals;
