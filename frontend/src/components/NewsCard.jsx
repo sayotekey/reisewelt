7aece6eebacc2823438f0cab05927a1835cd70e6
nataliia_petrukhina <natali.insajt@gmail.com>
2025-06-24 17:21:50 +0200

    Design der Karten hinzugefügt, Kommentare überarbeitet.

---
+++ frontend/src/components/NewsCard.jsx
@@ -0,0 +1,49 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion"; 
+
+const NewsCard = ({ _id, title, content, image, createdAt }) => { 
+  const formattedDate = new Date(createdAt).toLocaleDateString("de-DE", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
+  return (
+<motion.div 
+
+      initial={{ opacity: 0, y: 20 }} 
+      animate={{ opacity: 1, y: 0 }} 
+       whileHover={{ scale: 1.03 }}   
+      transition={{ duration: 0.4 }} 
+    >
+ <Link to={`/news/${_id}`}>
+  <div className="bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transition duration-300 h-80 flex flex-col">
+    
+    {/* ОБЁРТКА ДЛЯ КАРТИНКИ */}
+    <div className="h-40 overflow-hidden">
+      <img
+        src={image}
+        alt={title}
+        className="w-full h-full object-cover transition-transform duration-500 hover:scale-110"
+      />
+    </div>
+
+    <div className="p-4 flex flex-col justify-between flex-grow">
+      <div>
+        <h3 className="text-lg font-semibold text-gray-800 mb-2 line-clamp-2">
+          {title}
+        </h3>
+        <p className="text-sm text-gray-600 mb-2 line-clamp-2">{content}</p>
+      </div>
+      <p className="text-xs text-gray-400">{formattedDate}</p>
+    </div>
+  </div>
+</Link>
+
+    </motion.div>
+  );
+};
+
+export default NewsCard;
