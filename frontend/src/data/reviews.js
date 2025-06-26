85a0fd9ecfb20b2037238a02cd2f426d1320974e
nataliia_petrukhina <natali.insajt@gmail.com>
2025-06-26 13:05:53 +0200

    Frontend

---
+++ frontend/src/data/reviews.js
@@ -0,0 +1,30 @@
+export const reviews = [
+  {
+    id: 1,
+    name: "Johann Sosnowski",
+    date: "27.05.2025",
+    rating: 5,
+    initial: "J",
+    text: `Wir haben uns entschlossen unsere USA Reise bei CANUSA zu buchen. 
+Es war ein voller Erfolg! Herr Peter Vennemann, Büro Hürth(Köln), hat unsere Reise sehr gut geplant und unsere Wünsche berücksichtigt sodass wir ohne Komplikationen eine tolle Reise hatten.`,
+    color: "bg-teal-600",
+  },
+  {
+    id: 2,
+    name: "stefan W.",
+    date: "23.05.2025",
+    rating: 5,
+    initial: "S",
+    text: `Guten Morgen. Wir sitzen in Calgary im Airport Sandman Hotel und warten auf den Shuttle zum Wohnmobilvermittler...`,
+    color: "bg-blue-600",
+  },
+  {
+    id: 3,
+    name: "Erik Pfefferkorn",
+    date: "20.05.2025",
+    rating: 5,
+    initial: "E",
+    text: `Bei der Planung unserer Reise nach Hawaii wurde bestens auf unsere Wünsche eingegangen. Die Reise selbst war traumhaft schön...`,
+    color: "bg-purple-600",
+  },
+];
