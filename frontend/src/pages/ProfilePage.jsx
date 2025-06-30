0518048c6bdd64a445e344e8ecde60f09497f17a
Olena-prydalna <epridalnaya@gmail.com>
2025-06-30 15:53:49 +0200

    update ProfilePage to log user data and adjust layout; configure Vite server proxy settings

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -28,6 +28,8 @@ const ProfilePage = () => {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       });
+      console.log("User data:", res.data);
+      
       setLocalUser(res.data);
     } catch (error) {
       console.error("Error loading user data:", error);
@@ -185,7 +187,8 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex flex-row bg-gray-100">
+    <div className="min-h-screen w-full flex flex-row bg-gray-100 overflow-hidden">
+
       {/* Linkes Menü - Tabs für die Navigation */}
       <div className="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
         <div className="flex items-center justify-center h-20 shadow-md">
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss()]
+  tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 
 })
